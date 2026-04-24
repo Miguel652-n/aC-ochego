ea99357dd11a024d5202e9ce5c86f5748f208663
natalyftata-collab <Administrador@PUDLAB176896.senac.educ>
2026-04-24 16:56:18 -0300

    finalizada tela 1

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+
+
 function criarPet(nome, especie, genero, idade, regiao, ong, cidade, imagem) {
   return {
     nome,
@@ -13,51 +31,50 @@ function criarPet(nome, especie, genero, idade, regiao, ong, cidade, imagem) {
 
 const petsData = [
     // cachorros
-    criarPet("Ben", "cachorro", "macho", "Idoso", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Ben_I.png"),
-    criarPet("Babi", "cachorro", "femea", "Filhote", "MG", "Cão Viver", "São Paulo", "Assets/Doguinhos/Femeas/Babi_F.png"),
-    criarPet("Beto", "cachorro", "macho", "Idoso", "MG", "Amigo Fiel", "Niterói", "Assets/Doguinhos/Machos/Beto_I.png"),
-    criarPet("Bebel", "cachorro", "femea", "Adulto", "MG", "Amigo Fiel", "Niterói", "Assets/Doguinhos/Femeas/Bebel_A.png"),
-    criarPet("Bob", "cachorro", "macho", "Adulto", "MG", "Cão Viver", "São Paulo", "Assets/Doguinhos/Machos/Bob_A.png"),
+    criarPet("Ben", "cachorro", "macho", "Idoso", "MG", "Amparo pet", "Uberlândia", "Assets/Doguinhos/Machos/Ben_I.png"),
+    criarPet("Babi", "cachorro", "femea", "Filhote", "MG", "Cão Viver", "Contagem", "Assets/Doguinhos/Femeas/Babi_F.png"),
+    criarPet("Beto", "cachorro", "macho", "Idoso", "MG", "Amparo pet", "Uberlândia", "Assets/Doguinhos/Machos/Beto_I.png"),
+    criarPet("Bebel", "cachorro", "femea", "Adulto", "RJ", "Ação animal", "Rio de Janeiro", "Assets/Doguinhos/Femeas/Bebel_A.png"),
+    criarPet("Bob", "cachorro", "macho", "Adulto", "RJ", "Distrito Animal", "Rio de Janeiro", "Assets/Doguinhos/Machos/Bob_A.png"),
     criarPet("Bella", "cachorro", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Bella_A.png"),
-    criarPet("Caos", "cachorro", "macho", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Caos_A.png"),
-    criarPet("Collie", "cachorro", "femea", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Collie_F.png"),
-    criarPet("Chocolate", "cachorro", "macho", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Chocolate_A.png"),
-    criarPet("Jade", "cachorro", "femea", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Jade_F.png"),
-    criarPet("Cirius", "cachorro", "macho", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Cirius_F.png"),
-    criarPet("Lindinha", "cachorro", "femea", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Lindinha_F.png"),
-    criarPet("Dante", "cachorro", "macho", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Dante_A.png"),
+    criarPet("Caos", "cachorro", "macho", "Adulto", "RJ", "Cãodominio", "Resende", "Assets/Doguinhos/Machos/Caos_A.png"),
+    criarPet("Collie", "cachorro", "femea", "Filhote", "RJ", "Cãodominio", "Resende", "Assets/Doguinhos/Femeas/Collie_F.png"),
+    criarPet("Chocolate", "cachorro", "macho", "Adulto", "RJ", "Adote um Bichinho", "Itaguaí", "Assets/Doguinhos/Machos/Chocolate_A.png"),
+    criarPet("Jade", "cachorro", "femea", "Filhote", "RJ", "Adote um Bichinho", "Itaguaí", "Assets/Doguinhos/Femeas/Jade_F.png"),
+    criarPet("Cirius", "cachorro", "macho", "Filhote", "SP", "Alianima", "Bela vista", "Assets/Doguinhos/Machos/Cirius_F.png"),
+    criarPet("Lindinha", "cachorro", "femea", "Filhote", "SP", "Amor animal", "Marília", "Assets/Doguinhos/Femeas/Lindinha_F.png"),
+    criarPet("Dante", "cachorro", "macho", "Adulto", "SP", "Amor animal", "Marília", "Assets/Doguinhos/Machos/Dante_A.png"),
     criarPet("Luna", "cachorro", "femea", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Luna_F.png"),
-    criarPet("Fred", "cachorro", "macho", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Fred_F.png"),
-    criarPet("Malu", "cachorro", "femea", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Malu_F.png"),
-    criarPet("Hamlet", "cachorro", "macho", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Hamelet_F.png"),
-    criarPet("Mel", "cachorro", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Mel_A.png"),
-    criarPet("Kratus", "cachorro", "macho", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Kratus_A.png"),
-    criarPet("Panqueca", "cachorro", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Panqueca_A.png"),
-    criarPet("Nacho", "cachorro", "macho", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Nacho_A.png"),
-    criarPet("Tamires", "cachorro", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Tamires_A.png"),
-    criarPet("Thor", "cachorro", "macho", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Thor_A.png"),
-    criarPet("Zoe", "cachorro", "femea", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Zoe_F.png"),
-    criarPet("Zeus", "cachorro", "macho", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Machos/Zeus_F.png"),
-    // trocaR a cor da estrelinha da pacoca
-    criarPet("Paçoca", "cachorro", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Doguinhos/Femeas/Paçoca_A.png"),
+    criarPet("Fred", "cachorro", "macho", "Filhote", "RJ", "Adote um Bichinho", "Itaguaí", "Assets/Doguinhos/Machos/Fred_F.png"),
+    criarPet("Malu", "cachorro", "femea", "Filhote", "SP", "Alianima", "Bela vista", "Assets/Doguinhos/Femeas/Malu_F.png"),
+    criarPet("Hamlet", "cachorro", "macho", "Filhote", "SP", "Alianima", "Bela vista", "Assets/Doguinhos/Machos/Hamelet_F.png"),
+    criarPet("Mel", "cachorro", "femea", "Adulto", "GO", "Anjos da rua", "Goiânia", "Assets/Doguinhos/Femeas/Mel_A.png"),
+    criarPet("Kratus", "cachorro", "macho", "Adulto", "Go", "Anjos da rua", "Goiânia", "Assets/Doguinhos/Machos/Kratus_A.png"),
+    criarPet("Panqueca", "cachorro", "femea", "Adulto", "RJ", "Fazenda Modelo", "Guaratiba", "Assets/Doguinhos/Femeas/Panqueca_A.png"),
+    criarPet("Nacho", "cachorro", "macho", "Adulto", "GO", "Lar dos animais", "Goiânia", "Assets/Doguinhos/Machos/Nacho_A.png"),
+    criarPet("Tamires", "cachorro", "femea", "Adulto", "GO", "Anjos da rua", "Goiânia", "Assets/Doguinhos/Femeas/Tamires_A.png"),
+    criarPet("Thor", "cachorro", "macho", "Adulto", "GO", "Anjos da rua", "Goiânia", "Assets/Doguinhos/Machos/Thor_A.png"),
+    criarPet("Zoe", "cachorro", "femea", "Filhote", "RJ", "Cãodominio", "Resende", "Assets/Doguinhos/Femeas/Zoe_F.png"),
+    criarPet("Zeus", "cachorro", "macho", "Filhote", "SP", "Amor animal", "Marília", "Assets/Doguinhos/Machos/Zeus_F.png"),
+    criarPet("Paçoca", "cachorro", "femea", "Adulto", "SP", "Amor Animal", "Marília", "Assets/Doguinhos/Femeas/Paçoca_A.png"),
     // gatos  
-    criarPet("Cookie", "gato", "macho", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Miaus/Machos/Cookie_F.png"),
-    criarPet("Café", "gato", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Femeas/Cafe_A.png"),
+    criarPet("Cookie", "gato", "macho", "Filhote", "RJ", "Cãodominio", "Resende", "Assets/Miaus/Machos/Cookie_F.png"),
+    criarPet("Café", "gato", "femea", "Adulto", "RJ", "Cãodominio", "Resende", "Assets/Miaus/Femeas/Cafe_A.png"),
     criarPet("Coxinha", "gato", "macho", "Idoso", "MG", "Apa", "Uberlândia", "Assets/Miaus/Machos/Coxinha_I.png"),
-    criarPet("Faisca", "gato", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Femeas/Faisca_A.png"),
-    criarPet("Felix", "gato", "macho", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Miaus/Machos/Felix_F.png"),
-    criarPet("Flora", "gato", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Femeas/Flora_A.png"),
-    criarPet("Garfield", "gato", "macho", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Machos/Garfield_A.png"),
-    criarPet("Kitty", "gato", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Femeas/Kitty_A.png"),
-    criarPet("Gatito", "gato", "macho", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Miaus/Machos/Gatito_F.png"),
-    criarPet("Luz", "gato", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Femeas/Luz_A.png"),
+    criarPet("Faisca", "gato", "femea", "Adulto", "SP", "Amor animal", "Marília", "Assets/Miaus/Femeas/Faisca_A.png"),
+    criarPet("Felix", "gato", "macho", "Filhote", "SP", "Alianima", "Bela vista", "Assets/Miaus/Machos/Felix_F.png"),
+    criarPet("Flora", "gato", "femea", "Adulto", "MG", "Bichos Gerais", "Belo Horizonte", "Assets/Miaus/Femeas/Flora_A.png"),
+    criarPet("Garfield", "gato", "macho", "Adulto", "RJ", "Fazenda Modelo", "Guaratiba", "Assets/Miaus/Machos/Garfield_A.png"),
+    criarPet("Kitty", "gato", "femea", "Adulto", "Rj", "Fazenda modelo", "Guaratiba", "Assets/Miaus/Femeas/Kitty_A.png"),
+    criarPet("Gatito", "gato", "macho", "Filhote", "RJ", "Cãodominio", "Resende", "Assets/Miaus/Machos/Gatito_F.png"),
+    criarPet("Luz", "gato", "femea", "Adulto", "SP", "Ong SOS", "São Caetano do Sul", "Assets/Miaus/Femeas/Luz_A.png"),
     criarPet("Mingau", "gato", "macho", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Miaus/Machos/Mingau_F.png"),
-    criarPet("Maya", "gato", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Femeas/Maya_A.png"),
-    criarPet("Nico", "gato", "macho", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Machos/Nico_A.png"),
-    criarPet("Moon", "gato", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Femeas/Moon_A.png"),
-    criarPet("Ozzy", "gato", "macho", "Filhote", "MG", "Apa", "Uberlândia", "Assets/Miaus/Machos/Ozzy_F.png"),
-    criarPet("Pixie", "gato", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Femeas/Pixie_A.png"),
-    criarPet("Zoe", "gato", "femea", "Adulto", "MG", "Apa", "Uberlândia", "Assets/Miaus/Femeas/Zoe_A.png"),
+    criarPet("Maya", "gato", "femea", "Adulto", "MG", "Bichos gerais", "Belo Horizonte", "Assets/Miaus/Femeas/Maya_A.png"),
+    criarPet("Nico", "gato", "macho", "Adulto", "GO", "Anjos da rua", "Goiânia", "Assets/Miaus/Machos/Nico_A.png"),
+    criarPet("Moon", "gato", "femea", "Adulto", "Sp", "Ong SOS", "São Caetano do Sul", "Assets/Miaus/Femeas/Moon_A.png"),
+    criarPet("Ozzy", "gato", "macho", "Filhote", "SP", "Alianima", "Bela vista", "Assets/Miaus/Machos/Ozzy_F.png"),
+    criarPet("Pixie", "gato", "femea", "Adulto", "SP", "Amor animal", "Marília", "Assets/Miaus/Femeas/Pixie_A.png"),
+    criarPet("Zoe", "gato", "femea", "Adulto", "GO", "Lar dos animais", "Goinia", "Assets/Miaus/Femeas/Zoe_A.png"),
     
 ];
 
